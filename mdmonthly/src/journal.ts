9f916593af1c8ca5457a2432ea2dab8532600d35
Claudia <claui@users.noreply.github.com>
2023-04-24 09:17:21 +0200

    backend: implement default root dir

--- mdmonthly/src/journal.ts
+++ mdmonthly/src/journal.ts
@@ -2,8 +2,11 @@ import fs from "fs";
 import path from "path";
 
 import { Temporal } from "@js-temporal/polyfill";
+import envPaths from "env-paths";
 
 const { PlainDate } = Temporal;
+
+const BASE_NAME = "mdmonthly";
 
 export type JournalResult = {
   markdownFilePath: string;
@@ -12,23 +15,9 @@ export type JournalResult = {
   character: number;
 };
 
-export function createOrFindJournalEntry(
-  isoDate: string,
-  projectRoot: string,
-): JournalResult {
-  const date: Temporal.PlainDate = PlainDate.from(isoDate);
-  const year: string = date.year.toString();
-  const month: string = date.month.toString().padStart(2, "0");
-  const day: string = date.day.toString().padStart(2, "0");
-
-  const yearDirectory: string = path.join(projectRoot, year);
-  if (!fs.existsSync(yearDirectory)) {
-    fs.mkdirSync(yearDirectory);
-  }
-
+function readLines(year: string, month: string, yearDirectory: string) {
   const markdownFileName = `${year}-${month}.md`;
   const markdownFilePath: string = path.join(yearDirectory, markdownFileName);
-  const headerSnippet = `## ${year}-${month}-${day}\n\n`;
 
   if (!fs.existsSync(markdownFilePath)) {
     fs.writeFileSync(markdownFilePath, `# ${year}-${month}\n\n`);
@@ -36,6 +25,29 @@ export function createOrFindJournalEntry(
 
   const fileContent: string = fs.readFileSync(markdownFilePath, "utf8");
   const lines: string[] = fileContent.split("\n");
+  return { lines, markdownFilePath };
+}
+
+export function createOrFindJournalEntry(
+  isoDate: string,
+  projectRoot?: string,
+): JournalResult {
+  const paths = envPaths(BASE_NAME, { suffix: "" });
+
+  const date: Temporal.PlainDate = PlainDate.from(isoDate);
+  const year: string = date.year.toString();
+  const month: string = date.month.toString().padStart(2, "0");
+  const day: string = date.day.toString().padStart(2, "0");
+
+  const yearDirectory: string = path.join(projectRoot ?? paths.data, year);
+  if (!fs.existsSync(yearDirectory)) {
+    fs.mkdirSync(yearDirectory, { recursive: true });
+  }
+
+  const headerSnippet = `## ${year}-${month}-${day}\n\n`;
+  const { lines, markdownFilePath }:
+  { lines: string[]; markdownFilePath: string; }
+    = readLines(year, month, yearDirectory);
 
   let insertLine: number = -1;
   for (let i: number = 0; i < lines.length; i++) {
