02714cb7c736764e7b78236d09f02829a1e75a9b
Claudia <claui@users.noreply.github.com>
2023-06-05 18:06:12 +0200

    backend: extract methods

--- mdmonthly/src/journal.ts
+++ mdmonthly/src/journal.ts
@@ -31,49 +31,58 @@ function readLines(
   return { lines, markdownFilePath };
 }
 
+function splitDate(date: Temporal.PlainDate): {
+  year: string, month: string, day: string,
+} {
+  return {
+    year: date.year.toString(),
+    month: date.month.toString().padStart(2, "0"),
+    day: date.day.toString().padStart(2, "0"),
+  }
+}
+
+function calculateInsertLine(
+  lines: string[], date: Temporal.PlainDate): number {
+  for (let i: number = 0; i < lines.length; i++) {
+    if (lines[i].startsWith("## ")) {
+      const entryDate: Temporal.PlainDate
+        = PlainDate.from(lines[i].substring(3));
+      if (PlainDate.compare(entryDate, date) > 0) {
+        return i;
+      }
+    }
+  }
+
+  return lines.length - 1;
+}
+
+type JournalOptions = { projectRoot?: string, fs?: Fs, paths?: typeof Paths };
+
 export function createOrFindJournalEntry(
   isoDate: string,
   {
     projectRoot = void 0,
     fs = _fs,
     paths = Paths,
-  }: { projectRoot?: string, fs?: Fs, paths?: typeof Paths } = {},
+  }: JournalOptions = {},
 ): JournalResult {
   const date: Temporal.PlainDate = PlainDate.from(isoDate);
-  const year: string = date.year.toString();
-  const month: string = date.month.toString().padStart(2, "0");
-  const day: string = date.day.toString().padStart(2, "0");
-
+  const { year, month, day } = splitDate(date);
+  const headerSnippet = `## ${year}-${month}-${day}\n\n`;
   const yearDirectory: string = path.join(projectRoot ?? paths.dataPath, year);
+
   if (!fs.existsSync(yearDirectory)) {
     fs.mkdirSync(yearDirectory, { recursive: true });
   }
 
-  const headerSnippet = `## ${year}-${month}-${day}\n\n`;
   const { lines, markdownFilePath }:
   { lines: string[]; markdownFilePath: string; }
     = readLines(year, month, yearDirectory);
 
-  let insertLine: number = -1;
-  for (let i: number = 0; i < lines.length; i++) {
-    if (lines[i].startsWith("## ")) {
-      const entryDate: Temporal.PlainDate
-        = PlainDate.from(lines[i].substring(3));
-      if (PlainDate.compare(entryDate, date) > 0) {
-        insertLine = i;
-        break;
-      }
-    }
-  }
-
-  if (insertLine === -1) {
-    insertLine = lines.length - 1;
-  }
-
   return {
     markdownFilePath,
     headerSnippet,
-    line: insertLine,
+    line: calculateInsertLine(lines, date),
     character: 0,
   };
 }
